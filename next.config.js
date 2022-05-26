073f402587f006c88165b9d32211a46015d1a81d
sebastiangancino <sjgancino97sd@gmail.com>
2022-05-26 00:29:27 -0500

    Conectada Mac, continuamos clase 6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey:  'customValue', //permite tener los valores asignados para producción
+  },
+  basePath: 'dist',  //los documentos se generan en "dist"
+  compress: true,  // utiliza el protocolo gzip
+  async redirects(){
+    return [
+      {
+        source: '/hola',
+        destination: 'https://sebastiangano......',  //entra en hola y ser redirecciona a destination
+        permanent: true,
+      }
+    ]
+  } 
+
 }
 
 module.exports = nextConfig
